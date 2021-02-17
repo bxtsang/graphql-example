5a5a51c3d3dfa848184ab918d1b5cdef9bb3928e
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2021-02-17 22:16:02 +0800

    Move data to separate data.py file

--- app.py
+++ app.py
@@ -1,83 +1,9 @@
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 import requests
+from data import data
 
 app = Flask(__name__)
-
-data = {
-    "brands": [
-        {
-            "id": 1,
-            "name": "Apple"
-        },
-        {
-            "id": 2,
-            "name": "Huawei"
-        },
-        {
-            "id": 3,
-            "name": "Dell"
-        }
-    ],
-    "products": [
-        {
-            "id": 1,
-            "name": "Macbook Pro 16 inch",
-            "price": 3000,
-            "brand": 1,
-            "stock": 12,
-            "parent": None
-        },
-        {
-            "id": 2,
-            "name": "XPS 13",
-            "price": 2800,
-            "brand": 3,
-            "stock": 4,
-            "parent": None
-        },
-        {
-            "id": 3,
-            "name": "Matebook Pro",
-            "price": 2300,
-            "brand": 2,
-            "stock": 25,
-            "parent": None
-        },
-        {
-            "id": 4,
-            "name": "IPad Pro",
-            "price": 1500,
-            "brand": 1,
-            "stock": 3,
-            "parent": None
-        },
-        {
-            "id": 5,
-            "name": "Apple Pencil",
-            "price": 200,
-            "brand": 1,
-            "stock": 8,
-            "parent": 4
-        },
-        {
-            "id": 6,
-            "name": "IDock",
-            "price": 100,
-            "brand": 1,
-            "stock": 80,
-            "parent": 1
-        },
-        {
-            "id": 7,
-            "name": "The best IPad case",
-            "price": 50,
-            "brand": 1,
-            "stock": 100,
-            "parent": 4
-        }
-    ]
-}
 
 @app.route('/')
 def hello_world():
---
+++ data.py
@@ -0,0 +1,74 @@
+data = {
+    "brands": [
+        {
+            "id": 1,
+            "name": "Apple"
+        },
+        {
+            "id": 2,
+            "name": "Huawei"
+        },
+        {
+            "id": 3,
+            "name": "Dell"
+        }
+    ],
+    "products": [
+        {
+            "id": 1,
+            "name": "Macbook Pro 16 inch",
+            "price": 3000,
+            "brand": 1,
+            "stock": 12,
+            "parent": None
+        },
+        {
+            "id": 2,
+            "name": "XPS 13",
+            "price": 2800,
+            "brand": 3,
+            "stock": 4,
+            "parent": None
+        },
+        {
+            "id": 3,
+            "name": "Matebook Pro",
+            "price": 2300,
+            "brand": 2,
+            "stock": 25,
+            "parent": None
+        },
+        {
+            "id": 4,
+            "name": "IPad Pro",
+            "price": 1500,
+            "brand": 1,
+            "stock": 3,
+            "parent": None
+        },
+        {
+            "id": 5,
+            "name": "Apple Pencil",
+            "price": 200,
+            "brand": 1,
+            "stock": 8,
+            "parent": 4
+        },
+        {
+            "id": 6,
+            "name": "IDock",
+            "price": 100,
+            "brand": 1,
+            "stock": 80,
+            "parent": 1
+        },
+        {
+            "id": 7,
+            "name": "The best IPad case",
+            "price": 50,
+            "brand": 1,
+            "stock": 100,
+            "parent": 4
+        }
+    ]
+}
