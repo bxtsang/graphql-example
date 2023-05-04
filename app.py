6e2ee2d080f8258270894446d7b769fddca2cfff
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2023-05-05 02:35:20 +0800

    update routes to be more resty

--- app.py
+++ app.py
@@ -19,34 +19,34 @@ def hello_world():
 
 @app.route('/brands')
 def get_all_brands():
-    return jsonify(data.get_all_brands())
+    return jsonify(brands = data.get_all_brands())
 
 @app.route('/brands/<int:id>')
 def get_brand_by_id(id):
     brand = data.get_brand_by_id(id)
     if brand is not None:
-        return jsonify(brand)
+        return jsonify(brand = brand)
     return jsonify(error = "Brand id does not correspond with any products."), 404
 
 @app.route('/brands/name/<name>')
 def get_brands_by_name(name):
-    return jsonify(data.get_brands_by_name(name))
+    return jsonify(brands = data.get_brands_by_name(name))
 
 @app.route('/products')
 def get_all_products():
-    return jsonify(data.get_all_products())
+    return jsonify(products = data.get_all_products())
 
 @app.route('/products/<int:id>')
 def get_product_by_id(id):
     product = data.get_product_by_id(id)
     if product is not None:
-        return jsonify(product)
+        return jsonify(product = product)
 
     return jsonify(error = "Product id does not correspond with any products."), 404
 
 @app.route('/products/name/<name>')
 def get_products_by_name(name):
-    return jsonify(data.get_products_by_name(name))
+    return jsonify(products = data.get_products_by_name(name))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081, debug=True)
--- demo/src/main/java/com/example/demo/controllers/ProductController.java
+++ demo/src/main/java/com/example/demo/controllers/ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ProductController {
@@ -15,17 +17,17 @@ public class ProductController {
     ProductRepository productRepository;
 
     @GetMapping("/products")
-    public List<Product> getAllProducts() {
-        return productRepository.getAllProducts();
+    public Map<String, List<Product>> getAllProducts() {
+        return new HashMap<>() {{ put("products", productRepository.getAllProducts()); }};
     }
 
     @GetMapping("/products/{id}")
-    public Product getProductById(@PathVariable Integer id) {
-        return productRepository.getProduct(id);
+    public Map<String, Product> getProductById(@PathVariable Integer id) {
+        return new HashMap<>() {{ put("product", productRepository.getProduct(id)); }};
     }
 
     @GetMapping("/brands/{id}/products")
-    public List<Product> getProductByBrand(@PathVariable Integer id) {
-        return productRepository.getProductsByBrand(id);
+    public Map<String, List<Product>> getProductByBrand(@PathVariable Integer id) {
+        return new HashMap<>() {{ put("product", productRepository.getProductsByBrand(id)); }};
     }
 }
